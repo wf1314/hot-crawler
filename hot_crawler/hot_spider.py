adf0fa294a023dccd8a7e1b24be2495bdcdcefa8
孙明喆 <sunmingzhe@sunmingzhedeMacBook-Pro.local>
2019-08-01 19:48:12 +0800

    Update hot_spider.py

--- hot_crawler/hot_spider.py
+++ hot_crawler/hot_spider.py
@@ -58,44 +58,24 @@ class HotSpider(object):
             output.append(d)
         return output
 
-
     def weibo(self) -> list:
         """
         微博热搜爬取
         :return:
         """
-        
         url = 'https://s.weibo.com/top/summary?cate=realtimehot'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/75.0.3770.142 Safari/537.36',
-        }
+        r = requests.get(url)
+        doc = etree.HTML(r.text)
         output = []
-        html = etree.HTML(requests.get(url, headers=headers).text)
-        affair = html.xpath('//td[@class="td-02"]/a/text()')
-        Url = html.xpath('//td[@class="td-02"]/a/@href')
-
-        Url[0] = "https://s.weibo.com" + Url[0]
-
-        view = html.xpath('//td[@class="td-02"]/span/text()')
-        d = {
-            'title': affair[0],
-            'excerpt': "",
-            'metrics': "",
-            'link': Url[0],
-            'image': "",
-        }
-        output.append(d)
-        affair = affair[1:]
-        Url = Url[1:]
-        for o in range(0, len(affair)):
-            Url[o] = "https://s.weibo.com" + Url[o]
-
+        for div in doc.xpath('.//td[@class="td-02"]'):
+            affair = html.xpath('//td[@class="td-02"]/a/text()')[0]
+            Url = html.xpath('//td[@class="td-02"]/a/@href')[0]
+            view = html.xpath('//td[@class="td-02"]/span/text()')
             d = {
-                'title': affair[o],
+                'title': affair,
                 'excerpt': "",
-                'metrics': view[o],
-                'link': Url[o],
+                'metrics': "",
+                'link': Url,
                 'image': "",
             }
             output.append(d)
